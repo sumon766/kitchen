1a19394a002a87d8c564190c3f07b413da268ebb
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-10-31 18:31:49 +0600

    Header content part done - style remains

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,70 @@
-import '../stylesheets/Header.css';
+import "../stylesheets/Header.css";
 
 const Header = () => (
-    <div id="header">
-        <div className="container-fluid header-top">
-            <div className='container'>
-                <div className="row">
-                    <div className="col-md social-icons">
-                        <i class="fa fa-facebook" aria-hidden="true"></i>
-                        <i class="fa fa-twitter" aria-hidden="true"></i>
-                        <i class="fa fa-instagram" aria-hidden="true"></i>
-                        <i class="fa fa-youtube-play" aria-hidden="true"></i>
-                    </div>
-                    <div className="col-md header-links">
-                        <p>
-                            <i class="fa fa-file-text-o" aria-hidden="true"></i>
-                            Submit Recipe
-                        </p>
-                        <p>
-                            <i class="fa fa-heart-o" aria-hidden="true"></i>
-                            Saved Recipes
-                        </p>
-                        <p>
-                            <i class="fa fa-lock" aria-hidden="true"></i>
-                            Signup/Login
-                        </p>
-                    </div>
-                </div>
-            </div>
+  <div id="header">
+    <div className="container-fluid header-top">
+      <div className="container">
+        <div className="row">
+          <div className="col-md social-icons">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+            <i class="fa fa-twitter" aria-hidden="true"></i>
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+            <i class="fa fa-youtube-play" aria-hidden="true"></i>
+          </div>
+          <div className="col-md header-links">
+            <p>
+              <i class="fa fa-file-text-o" aria-hidden="true"></i>
+              Submit Recipe
+            </p>
+            <p>
+              <i class="fa fa-heart-o" aria-hidden="true"></i>
+              Saved Recipes
+            </p>
+            <p>
+              <i class="fa fa-lock" aria-hidden="true"></i>
+              Signup/Login
+            </p>
+          </div>
         </div>
+      </div>
     </div>
+
+    <div className="container">
+      <div className="row">
+        <div className="logo text-center">
+          <img src="/images/logo.png" />
+        </div>
+      </div>
+    </div>
+
+    <div className="container-fluid">
+      <div className="container">
+        <nav class="navbar navbar-expand-sm justify-content-center">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Link 1
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Link 2
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Link 3
+              </a>
+            </li>
+          </ul>
+        </nav>
+        {/* The following search icon part is incomplete */}
+        {/* <div className="search-icon">
+            <i class="fa fa-search" aria-hidden="true"></i>
+        </div> */}
+      </div>
+    </div>
+  </div>
 );
 
 export default Header;
