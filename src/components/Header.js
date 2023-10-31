00dbaa3496a7c407d6e44af9e0c2a7824b4cee62
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-10-31 19:11:00 +0600

    Fixed className related issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,22 +6,22 @@ const Header = () => (
       <div className="container">
         <div className="row">
           <div className="col-md social-icons">
-            <i class="fa fa-facebook" aria-hidden="true"></i>
-            <i class="fa fa-twitter" aria-hidden="true"></i>
-            <i class="fa fa-instagram" aria-hidden="true"></i>
-            <i class="fa fa-youtube-play" aria-hidden="true"></i>
+            <i className="fa fa-facebook" aria-hidden="true"></i>
+            <i className="fa fa-twitter" aria-hidden="true"></i>
+            <i className="fa fa-instagram" aria-hidden="true"></i>
+            <i className="fa fa-youtube-play" aria-hidden="true"></i>
           </div>
           <div className="col-md header-links">
             <a href='/submi-recipe'>
-              <i class="fa fa-file-text-o" aria-hidden="true"></i>
+              <i className="fa fa-file-text-o" aria-hidden="true"></i>
               Submit Recipe
             </a>
             <a href="/saved-recipes">
-              <i class="fa fa-heart-o" aria-hidden="true"></i>
+              <i className="fa fa-heart-o" aria-hidden="true"></i>
               Saved Recipes
             </a>
             <a href="/login">
-              <i class="fa fa-lock" aria-hidden="true"></i>
+              <i className="fa fa-lock" aria-hidden="true"></i>
               Login
             </a>
           </div>
@@ -39,20 +39,20 @@ const Header = () => (
 
     <div className="container-fluid">
       <div className="container">
-        <nav class="navbar navbar-expand-sm justify-content-center">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="#">
+        <nav className="navbar navbar-expand-sm justify-content-center">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
                 Link 1
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
                 Link 2
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
                 Link 3
               </a>
             </li>
