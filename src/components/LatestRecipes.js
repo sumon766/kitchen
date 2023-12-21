feb6b65bd5cd8b65ce9f5ef983c3b10fdb050383
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-12-21 16:28:57 +0600

    Setup loading and error messages

--- src/components/LatestRecipes.js
+++ src/components/LatestRecipes.js
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from "react";
 
 const LatestRecipes = () => {
     const [recipes, setRecipes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         const apiUrl = "https://api.edamam.com/api/food-database/v2/parser?app_id=bfcc42d1&app_key=79dc372926e730783e6ed602b6c76503&nutrition-type=cooking";
@@ -12,8 +14,11 @@ const LatestRecipes = () => {
                 const json = await response.json();
                 console.log(json.hints);
                 setRecipes(json.hints);
+                setLoading(false);
             } catch (error) {
                 console.log(error);
+                setError("Something went wrong, please contact the site administrator")
+                setLoading(false);
             }
         }
 
@@ -23,16 +28,25 @@ const LatestRecipes = () => {
     return (
         <>
             <div>Here is the food data</div>
-            <div className="recipes">
-                <ul>
-                   {recipes.map((recipe, index) => (
-                        <li key={index}>
-                            <h3>{recipe.food.label}</h3>
-                            <p>Category: {recipe.food.category}</p>
-                        </li>
-                   ))}
-                </ul>
-            </div>
+
+            {loading && <p>Loading data...</p>}
+
+            {error && <p>{error}</p>}
+
+            {!loading && !error && (
+                <div className="recipes">
+                    <ul>
+                    {recipes.map((recipe, index) => (
+                            <li key={index}>
+                                <h3>{recipe.food.label}</h3>
+                                <p>Category: {recipe.food.category}</p>
+                            </li>
+                    ))}
+                    </ul>
+                </div>
+            )}
+
+            
         </>
     );
 }
